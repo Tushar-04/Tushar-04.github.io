c1ec65c71c76713eae2554f4e32a98876e3b7b23
Tushar <s.v.tushar04@gmail.com>
2022-04-17 10:34:11 +0530

    sort

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,26 +3,27 @@ import { useEffect, useState } from "react";
 import './Serach_bar.css';
 import SearchIcon from "@material-ui/icons/Search";
 import Page1 from './Page1';
-
+import { DropdownButton } from 'react-bootstrap'
+import { Dropdown } from 'react-bootstrap'
 export default function SearchBar() {
     const [repoList, SetrepoList] = useState([]);
-    const GetrepoList = async (l,n,s,o,p,pi) => {
-        if(l!="" && n==""){
+    const GetrepoList = async (l, n, s, o, p, pi) => {
+        if (l != "" && n == "") {
             const temp = await fetch(`https://api.github.com/search/repositories?q=language:${l}&sort=${s}&order=${o}&page=${p}&per_page=${pi}`).then(res => res.json());
             SetrepoList(temp.items);
         }
-        else if(l=="" && n!=""){
+        else if (l == "" && n != "") {
             const temp = await fetch(`https://api.github.com/search/repositories?q=name:${n}&sort=${s}&order=${o}&page=${p}&per_page=${pi}`).then(res => res.json());
             SetrepoList(temp.items);
         }
-        else if(l!="" && n!=""){
+        else if (l != "" && n != "") {
             const temp = await fetch(`https://api.github.com/search/repositories?q=language:${l}&name:${n}&sort=${s}&order=${o}&page=${p}&per_page=${pi}`).then(res => res.json());
             SetrepoList(temp.items);
         }
-        
+        console.log(o);
     };
     const handleSearch = () => {
-        GetrepoList(wordEnter,sname,sort,order,currentPage,postsPerPage)
+        GetrepoList(wordEnter, sname, sort, order, currentPage, postsPerPage)
     };
     const [wordEnter, setWordEnter] = useState("");
     const handleFilter = event => {
@@ -39,13 +40,21 @@ export default function SearchBar() {
         const enteredWord = event.target.value;
         setCurrentPage(enteredWord)
     };
-    const [postsPerPage,setpostsPerPage] = useState("");
+    const [postsPerPage, setpostsPerPage] = useState("");
     const handlepostsPerPage = event => {
         const enteredWord = event.target.value;
         setpostsPerPage(enteredWord)
     };
-    const [sort,setsort] = useState("stars");
-    const [order,setorder] = useState("desc");
+    const [sort, setsort] = useState("stars");
+    const [order, setorder] = useState("desc");
+    const changeoder=(o)=>{
+        setorder(o);
+        GetrepoList(wordEnter, sname, sort, o, currentPage, postsPerPage);
+    }
+    const changesort=(s)=>{
+        setsort(s);
+        GetrepoList(wordEnter, sname, sort, order, currentPage, postsPerPage);
+    }
     return (
         <>
             <div className="search">
@@ -63,8 +72,20 @@ export default function SearchBar() {
                 <div className="searchInputs">
                     <div className="icon" onClick={handleSearch}><SearchIcon /></div>
                 </div>
-
+                <div className='drop'>
+                {repoList.length !== 0 &&(
+                <><DropdownButton id="dropdown-basic-button" title="Sort by">
+                    <Dropdown.Item onClick={()=>{changesort("star");}}>Stars</Dropdown.Item>
+                    <Dropdown.Item onClick={()=>{changesort("name");}}>Name</Dropdown.Item>
+                </DropdownButton>
+                <DropdownButton id="dropdown-basic-button" title="Order">
+                    <Dropdown.Item onClick={()=>{changeoder("asc");}}>Ascending</Dropdown.Item>
+                    <Dropdown.Item onClick={()=>{changeoder("desc");}}>Descending</Dropdown.Item>
+                </DropdownButton>
+                </>)}
+                </div>                
             </div>
+
             {repoList.length !== 0 && (<Page1 title={repoList} />)}
         </>
     );
